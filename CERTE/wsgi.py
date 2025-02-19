457294aa4aeb2252fcfe72c86e744a1b25ef29d7
malli1234759 <mannem.mallikarjunarao@ltimindtree.com>
2025-02-19 14:34:11 +0530

    Update wsgi.py

--- CERTE/wsgi.py
+++ CERTE/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CERTE.settings')
 
+# Create the WSGI application
 application = get_wsgi_application()
+
+# For Vercel, you may need to define the handler
+handler = application  # This line is crucial for Vercel
